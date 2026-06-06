d11ca8ca26715f98d4cd7c8372b1e12b96f00f62
mishbhav <bhavyamishra2000@gmail.com>
2026-06-06 20:41:55 +0530

    get cart from backend

--- ecomm/ecommerce/src/pages/HomePage.jsx
+++ ecomm/ecommerce/src/pages/HomePage.jsx
@@ -5,10 +5,16 @@ import './HomePage.css'
 
 export function HomePage() {
     const [products, setProducts]= useState([]);
+    const [cart, setCart]= useState([]);
     useEffect(() => {
         axios.get('http://localhost:3000/api/products')
             .then((response) => {
                 setProducts(response.data);
+            });
+
+        axios.get('http://localhost:3000/api/cart-items')
+            .then((response) => {
+                setCart(response.data);
             });
     }, []);
 
@@ -16,7 +22,7 @@ export function HomePage() {
 
         <>
             <title>ECommerce</title>
-            <Header />
+            <Header cart={cart} />
 
 
 
