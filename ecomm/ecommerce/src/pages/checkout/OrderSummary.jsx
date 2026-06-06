a7e6f7341ee7ed752e9124325c379a89c4fa6fd5
mishbhav <bhavyamishra2000@gmail.com>
2026-06-07 01:05:54 +0530

    cleaned Order page

--- ecomm/ecommerce/src/pages/checkout/OrderSummary.jsx
+++ ecomm/ecommerce/src/pages/checkout/OrderSummary.jsx
@@ -1,6 +1,7 @@
 import dayjs from 'dayjs';
 import { formatMoney } from '../../utils/money'
-export function OrderSummary({deliveryOptions, cart}) {
+import { DeliveryOptions } from './DeliveryOptions';
+export function OrderSummary({ deliveryOptions, cart }) {
     return (
 
         <div className="order-summary">
@@ -38,34 +39,7 @@ export function OrderSummary({deliveryOptions, cart}) {
                                     </span>
                                 </div>
                             </div>
-
-                            <div className="delivery-options">
-                                <div className="delivery-options-title">
-                                    Choose a delivery option:
-                                </div>
-                                {deliveryOptions.map((deliveryOption) => {
-                                    let priceShipping = 'FREE Shipping';
-                                    if (deliveryOption.priceCents > 0) {
-                                        priceShipping = `${formatMoney(deliveryOption.priceCents)} - Shipping`
-                                    }
-                                    return (
-                                        <div key={deliveryOption.id} className="delivery-option">
-                                            <input type="radio"
-                                                checked={deliveryOption.id === cartItem.deliveryOptionId}
-                                                className="delivery-option-input"
-                                                name={`delivery-option-${cartItem.productId}`} />
-                                            <div>
-                                                <div className="delivery-option-date">
-                                                    {dayjs(deliveryOption.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
-                                                </div>
-                                                <div className="delivery-option-price">
-                                                    {priceShipping}
-                                                </div>
-                                            </div>
-                                        </div>
-                                    );
-                                })}
-                            </div>
+                        <DeliveryOptions deliveryOptions={deliveryOptions} cartItem={cartItem} />
                         </div>
                     </div>
                 );
--- ecomm/ecommerce/src/pages/orders/Orders.jsx
+++ ecomm/ecommerce/src/pages/orders/Orders.jsx
@@ -1,8 +1,7 @@
 import axios from 'axios';
-import dayjs from 'dayjs';
-import { useState, useEffect, Fragment } from 'react';
+import { useState, useEffect } from 'react';
 import { Header } from '../../components/Header';
-import { formatMoney } from '../../utils/money';
+import { Order } from './Order';
 import './Orders.css';
 
 export function Orders({ cart }) {
@@ -28,60 +27,7 @@ export function Orders({ cart }) {
           {orders.map((order) => {
             return (
               <div key={order.id} className="order-container">
-
-                <div className="order-header">
-                  <div className="order-header-left-section">
-                    <div className="order-date">
-                      <div className="order-header-label">Order Placed:</div>
-                      <div>{dayjs(order.orderTimeMs).format('MMMM D')}</div>
-                    </div>
-                    <div className="order-total">
-                      <div className="order-header-label">Total:</div>
-                      <div>{formatMoney(order.totalCostCents)}</div>
-                    </div>
-                  </div>
-
-                  <div className="order-header-right-section">
-                    <div className="order-header-label">Order ID:</div>
-                    <div>{order.id}</div>
-                  </div>
-                </div>
-
-                <div className="order-details-grid">
-                  {order.products.map((orderProduct) => {
-                    return (
-                      <Fragment key={orderProduct.product.id}>
-                        <div className="product-image-container">
-                          <img src={orderProduct.product.image} />
-                        </div>
-
-                        <div className="product-details">
-                          <div className="product-name">
-                            {orderProduct.product.name}
-                          </div>
-                          <div className="product-delivery-date">
-                            Arriving on: {dayjs(orderProduct.estimatedDeliveryTimeMs).format('MMMM D')}
-                          </div>
-                          <div className="product-quantity">
-                            Quantity: {orderProduct.quantity}
-                          </div>
-                          <button className="buy-again-button button-primary">
-                            <img className="buy-again-icon" src="images/icons/buy-again.png" />
-                            <span className="buy-again-message">Add to Cart</span>
-                          </button>
-                        </div>
-
-                        <div className="product-actions">
-                          <a href="/tracking">
-                            <button className="track-package-button button-secondary">
-                              Track package
-                            </button>
-                          </a>
-                        </div>
-                      </Fragment>
-                    );
-                  })}
-                </div>
+                <Order order={order} />
               </div>
             );
           })}
